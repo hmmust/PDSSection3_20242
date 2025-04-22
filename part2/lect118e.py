6fb2b0c0207b7bf2d78889ad4def41818a2c4ddb
hossam <hossa@uop.edu.jo>
2025-04-22 12:51:18 +0300

    fillna, dropna

--- part2/lect118e.py
+++ part2/lect118e.py
@@ -3,5 +3,4 @@ students= np.array(['Khalid','Lana','Mira','Zaid','Mohammad','Baryhan'])
 print(np.random.choice(students))
 print(np.random.choice(students,size=2))
 print(np.random.choice(students,size=(2,2)))
-
 print(np.random.choice(students,size=(7,2)))
---
+++ part3/lect20.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+student_df = pd.read_csv('./part3/students.csv')
+print(student_df.info())
+print(student_df.isnull())
+print(student_df.isnull().sum())
+print(student_df.notnull().sum())
+print(student_df.Year.isnull().sum())
+#student_df.Year.fillna(0,inplace=True)
+#student_df.Year.fillna(method='pad',inplace=True)
+#student_df.Year.fillna(method='backfill',inplace=True)
+student_df.Year.fillna(student_df['Year'].median(),inplace=True)
+#student_df.Year.fillna(student_df['Year'].mean(),inplace=True)
+#student_df.dropna(inplace=True)
+#student_df.dropna(axis=1, inplace=True)
+student_df.Major=student_df.Major.str.strip()
+student_df.Major.replace({"DSS":'DS'}, #' SE':'SE'                      },
+                         inplace=True)
+print(student_df.Major.value_counts())
+
+
+
+
+
